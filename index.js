f65c564af531d25ce10b1a4446c60adc1e3351b3
rohan <rohankademani@outlook.in>
2021-08-11 09:36:07 +0530

    updated

---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+//Blocking Synchronus
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is a what we know about avacado: ${textIn} Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log("File written successfully.");
+
+//this is a comment
+
+//Non blocking Asynchornus
+// fs.readFile("./txt/start.txt", 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{    
+//         fs.writeFile('./txt/fianl.txt',`${data2}\n${data3}`,'utf-8',()=>{
+//             console.log("File written successfully");
+//         })
+//         })
+//     })
+
+// })
+// console.log("File is being reading");
+///////////////////////////////////
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs); 
+
+//SERVER
+const server = http.createServer((req, res) => {
+    let { query, pathname } = url.parse(req.url, true);
+    const pathName = pathname;
+    query = { ...query };
+    if (pathName === '/' || pathName === '/overview') {
+        res.writeHead((200), {
+            'Content-type': "text/html"
+        });
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+        res.end(output);
+    }
+    else if (pathName === "/product") {
+        res.writeHead((200), {
+            'Content-type': "text/html"
+        });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+    }
+    else if (pathName == '/api') {
+        res.writeHead((200), {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
+    }
+    else {
+        res.writeHead((404), {
+            'Content-type': 'text/html',
+            'Myheader': 'hello'
+        });
+        res.end("<H1>404 not found</H1>");
+    }
+});
+server.listen(8000, '127.0.0.1', () => {
+    console.log("Listening to request on port 8000!");
+});
